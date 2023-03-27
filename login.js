c419e54e255c0b9656acd54f63aedbb97703c92e
parkhy0sh1n <parkhy0sh1n@naver.com>
2023-03-27 19:48:00 +0900

    2023-03-27

---
+++ login.js
@@ -0,0 +1,30 @@
+var idPass = false;
+
+$('#id').on('keyup', function() {
+    // 아이디 입력란에 입력된 값 : $(this).val() == this.value
+    let idValue = $(this).val();
+    if(idValue.length == 0) {
+        $('#msg_id').text('');
+        idPass = false;
+    } else if(idValue.length < 4) {
+        $('#msg_id').text('아이디는 4자 이상입니다.').css('color', '#FF0000').css('font-size', '14px');
+        ifPass = false;
+    } else {
+        $('#msg_id').text('사용 가능한 아이디입니다.').css('color', '#0000FF').css('font-size', '14px');
+        idPass = true;
+    }
+    // idPass = idValue.length >= 4
+})
+
+$('#frm_login').on('submit', function(event) {
+    if(idPass == false) {
+        alert('아이디를 확인하세요.');
+        event.preventDefault();
+        return;
+    }
+    if($('#pw').val() == '') {
+        alert('비밀번호를 입력하세요.');
+        event.preventDefault();
+        return;
+    }
+})
